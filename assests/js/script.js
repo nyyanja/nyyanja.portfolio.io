4c0f0583ec5f9637cf8119cdf7e030838f8ba1e3
nyyanja <nyanja.ramahavatra@gmail>
2026-01-12 07:26:27 +1100

    modified css

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,16 @@
-// cv
-   function showCV() {
-      const frame = document.getElementById("cvFrame");
-      frame.style.display = "block";
-      frame.scrollIntoView({ behavior: "smooth" });
+ function toggleMobileMenu() {
+    const menu = document.getElementById('mobile-menu');
+    const panel = document.getElementById('mobile-menu-panel');
+
+    if (menu.classList.contains('hidden')) {
+      menu.classList.remove('hidden');
+      setTimeout(() => {
+        panel.classList.remove('translate-x-full');
+      }, 10); // petit délai pour déclencher l’animation
+    } else {
+      panel.classList.add('translate-x-full');
+      setTimeout(() => {
+        menu.classList.add('hidden');
+      }, 300); // attendre que l’animation se termine
     }
+  }
